b1fc55bb25aec499ee5ba18e46dae7576c3b00db
Pichborith Kong <pichborith@gmail.com>
2023-04-13 19:13:31 -0700

    move tempResults state to app component so when click anywhere the suggestion gone

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Feature, Footer, Loading, Preview, Search, Title } from './components';
 import { useAppSelector } from './app/hooks';
+import { useState } from 'react';
+import { Record } from './types/types';
 
 function App() {
   const isLoading = useAppSelector((state) => state.searchResult.loading);
+  const [tempResults, setTempResults] = useState<Record[]>([]);
 
   return (
     <>
-      <div id='app'>
+      <div id='app' onClick={() => setTempResults([])}>
         <Title />
-        <Search />
+        <Search tempResults={tempResults} setTempResults={setTempResults} />
         <Preview />
         <Footer />
         {isLoading && <Loading />}
